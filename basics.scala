cd6719f8f8d08360f58fcaad87ff56e5353f7582
Asif Sayyed <asifdotexe@gmail.com>
2024-07-24 08:04:18 +0530

    #1 trying out variable assignment

---
+++ basics.scala
@@ -0,0 +1,15 @@
+// variables
+var a: Int = 5 // var a: Int = 5
+
+var b = 5 // var b: Int = 5
+
+var b = 5.5 // var b: Double = 5.5
+
+var b = 5.5f // var b: Float = 5.5
+
+var c = a + b // var c: Float = 10.5
+
+var c = b + a // var c: Float = 10.5
+
+var c = a + 10.9 // var c: Double = 15.9
+
