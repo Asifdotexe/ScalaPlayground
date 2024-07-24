79f82df59be4865ae180f5a3315306d15a480830
Asif Sayyed <asifdotexe@gmail.com>
2024-07-24 08:05:20 +0530

    #1 trying out strings

--- basics.scala
+++ basics.scala
@@ -13,3 +13,22 @@ var c = b + a // var c: Float = 10.5
 
 var c = a + 10.9 // var c: Double = 15.9
 
+// String
+
+var name = 'Asif' // will not work
+/* -- Error: ----------------------------------------------------------------------
+1 |var name = 'Asif'
+  |           ^
+  |symbol literal 'Asif is no longer supported,
+  |use a string literal "Asif" or an application Symbol("Asif") instead,
+  |or enclose in braces '{Asif} if you want a quoted expression.
+  |For now, you can also `import language.deprecated.symbolLiterals` to accept
+  |the idiom, but this possibility might no longer be available in the future.
+-- Error: ----------------------------------------------------------------------
+1 |var name = 'Asif'
+  |                ^
+  |                unclosed character literal */
+
+
+var name = "Asif" // output:var name: String = Asif
+
