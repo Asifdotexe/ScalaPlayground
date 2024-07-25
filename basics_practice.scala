925fc43a0a7e53bec345e7036bf4dd10397b4ab5
Asif Sayyed <asifdotexe@gmail.com>
2024-07-25 18:41:03 +0530

    #4 practiced variables

--- basics_practice.scala
+++ basics_practice.scala
@@ -1,3 +1,13 @@
+/* Variables */
+
 // Create a val for an immutable variable and a var for a mutable variable.
 var mutable_variable = 5
 val immutable_variable = 10
+
+// Define an immutable variable x with the value of 10
+val x = 10 // output: val x: Int = 10
+
+// Define a mutable variable y with the value of 20 and change it to 25
+var y = 20 // output: val y: Int = 20
+var y = 25 // output: val y: Int = 25
+
