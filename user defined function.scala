f8781de89a72622456b16772b6cb52d9d969831f
Asif Sayyed <asifdotexe@gmail.com>
2024-07-27 10:40:16 +0530

    #6 defined getNumberSum function

--- user defined function.scala
+++ user defined function.scala
@@ -6,3 +6,37 @@ def incrementer(input_number: Int): Unit = {
     number += 1
     }
 }
+
+def helloWorld(name: String): Unit = {
+    println(s"Hello, My name is $name!")
+}
+
+def helloDarling(name: String, partner_name: String): Unit = {
+    println(s"Hello, My name is $name! My home is, $partner_name and I extremely adore her!")
+}
+
+/* Practice */
+
+a = 1478
+
+def getNumberSum(input_number: Int): Unit {
+    total = 0
+    var number = input_number
+    var num_string = number.toString
+    var length = num_string.length
+    for (i <- 0 to length - 1) {
+        total = total+= num_string(i).toInt
+    }
+}
+
+def getNumberSum(input_number: Int): Int = {
+  var total = 0
+  val num_string = input_number.toString
+  val length = num_string.length
+  
+  for (i <- 0 until length) {
+    total += num_string(i).asDigit
+  }
+
+  total
+}
