07ac3d6310593d7cc105a0695c39e1745c968c7b
Asif Sayyed <asifdotexe@gmail.com>
2024-07-27 10:12:40 +0530

    #4 performing a while loop

--- loops.scala
+++ loops.scala
@@ -1,7 +1,35 @@
 /* 
     Loops 
     - Do While Loops: For unknown interval of times, executes once -> runs until the condition is true
+
+        do {
+            statements
+        } while (condition)
+
     - While Loops: For unknown interval of times, runs until the condition is true
+
+        while (condition) {
+            statements
+        }
+
     - For Loops: For fixed interval of time
     - For each Loop: For working with collections (List, Tuple, Map, Array, Set, Factors)
 */
+
+// do while Loops
+a = 15
+
+do { 
+    println("Starting the loop") 
+    println(a)
+    a += 1
+} while (a <= 10)
+
+// while loops
+var a = 1 
+
+while (a <= 10) { 
+    println("Loop started")
+    println(a)
+    a += 1
+}
