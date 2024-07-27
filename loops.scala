6442dc7ba1ecc998ed55c0016de25f51ef610d3a
Asif Sayyed <asifdotexe@gmail.com>
2024-07-27 09:52:29 +0530

    #6 added defination

---
+++ loops.scala
@@ -0,0 +1,7 @@
+/* 
+    Loops 
+    - Do While Loops: For unknown interval of times, executes once -> runs until the condition is true
+    - While Loops: For unknown interval of times, runs until the condition is true
+    - For Loops: For fixed interval of time
+    - For each Loop: For working with collections (List, Tuple, Map, Array, Set, Factors)
+*/
