a8259633d9a8a9ce2b0f19ab8c475bee8397da54
Asif Sayyed <asifdotexe@gmail.com>
2024-08-06 09:28:54 +0530

    #8 added ds and array file

---
+++ data_structures.scala
@@ -0,0 +1,2 @@
+var newList = List(1,2,3,4,5,6) // output: var newList: List[Int] = List(1, 2, 3, 4, 5, 6)
+var anotherList = List(7,8,8,9,10) // output: var anotherList: List[Int] = List(7, 8, 8, 9, 10)
