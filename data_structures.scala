96d899b0df62f6a0f2545416c241fe4d4b6c1182
Asif Sayyed <asifdotexe@gmail.com>
2024-08-06 09:34:45 +0530

    #8 performed for loop on List

--- data_structures.scala
+++ data_structures.scala
@@ -1,2 +1,11 @@
 var newList = List(1,2,3,4,5,6) // output: var newList: List[Int] = List(1, 2, 3, 4, 5, 6)
 var anotherList = List(7,8,8,9,10) // output: var anotherList: List[Int] = List(7, 8, 8, 9, 10)
+
+print(newList(1))
+
+for (i <- 0 until newList.size){
+    println(newList(i))
+}
+
+// one line defination
+for (i <- 0 until newList.size) println(newList(i))
