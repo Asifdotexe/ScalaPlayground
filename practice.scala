aa49617cdcb7e257b6783c8ce143d7f046aca9fb
Asif Sayyed <asifdotexe@gmail.com>
2024-08-18 12:27:59 +0530

    #10 wrote a algo and program for fibonacci and palindrome

--- practice.scala
+++ practice.scala
@@ -1,58 +1,33 @@
-// 1. Write a scala program to print fibonacci series up to 100
+// WRITE A SCALA PRHRAM TO PRINT THE FIBONACCI SERIES UP TO 100.
+----------------------------------------------------------------
+// ALGORITHM
+    // 1. Initialize the first two number of the series as a = 0 and b = 1
+    // 2. Print 'a'
+    // 3. Calculate the next number as the sum of 'a' and 'b'
+    // 4. Update 'a' as 'b' and 'b' as the next number
+    // 5. Repeat the step 2-4 while 'a' is less than or equal to 100.
 
-val n = 100
-var a = 0
-var b = 1
+var target: Int = 100
+var a: Int = 0
+var b: Int = 1
 
-while (a <= n) {
-    println(a)
-    val next = a + b
-    a = b
-    b = next
+while (a < target) {
+    print(a)
+    val nextNumber = a + b
+    a = b 
+    b = nextNumber
 }
 
-// 2. Write a Scala program to find wheather a string is palindrome or Not
+// WRITE A SCALA PROGRAM TO FIND WHETHER A STRING IS PALINDROME OR NOT 
+----------------------------------------------------------------
+// ALGORITHM 
+    // 1. Define a fucntion that takes a string as input.
+    // 2. Reverse the string 
+    // 3. Check if the reverse string matches the original string 
+    // 4. Return true if they match otherwise return false
 
-def isPalindrome(str: String): Boolean = {
-    var input_str = str
-    input_str == input_str.reverse
+def isPalindrome(text: String): Boolean = {
+    val reversedText = text.reverse
+    reversedText == text
 }
 
-// 3. Write a scala program to display first 10 natural numbers and their sum
-
-var sum = 0
-for (i <- 1 until 11) {
-    sum += i
-    println(i)
-}
-print(sum)
-
-// 4. Write a Scala program fidn that entered year is leap year or not
-
-def find_leap_year(year: Int) = {
-    var input_year = year
-    if ((input_year % 4 == 0 && input_year % 100 != 0) || (input_year % 400 == 0)) {
-        print(s"$input_year is a leap year")
-    } else {
-        print(s"$input_year is not a leap year")
-    }
-}
-
-// 5. Write a scala program to check if number is a prime number
-
-import scala.math.sqrt
-def find_prime_number(input_number: Int) = {
-
-    var num = input_number
-    var numsqrt = sqrt(num.toInt)
-
-    for (i <- 2 until numsqrt.toInt) {
-        if (num % i == 0) {
-            println("It is a not prime number")
-        } else {
-            println("It is a prime number")
-        }
-    }
-}
-
-// Write a scala program to reverse a given number
